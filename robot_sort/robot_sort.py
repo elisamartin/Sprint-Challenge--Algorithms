a3cb8bd902d6b14d28c79c31a735daa0b7cacf2f
elisamartin <elisamv93@gmail.com>
2019-06-07 11:39:10 +0200

    MVP done

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -102,7 +102,6 @@ class SortingRobot:
         SortingRobot.set_light_on(self)
         while SortingRobot.light_is_on(self):
             SortingRobot.set_light_off(self)
-            SortingRobot.swap_item(self)
             while SortingRobot.can_move_right(self):
                 SortingRobot.move_right(self)
                 if SortingRobot.compare_item(self) == 1:
